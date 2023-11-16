138df62b5f326197daa8d46e5d07bff63073f380
yerinlee <dpfls0922@naver.com>
2023-11-17 01:02:07 +0900

    [Add] #11 내 프로필 모델 추가

--- mypage/models.py
+++ mypage/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
+
+class Profile(models.Model):
+    image = models.ImageField(verbose_name='이미지', blank=True, default='/default.jpg')
+    email = models.EmailField(verbose_name='이메일', max_length=128)
+    password = models.CharField(verbose_name='비밀번호', max_length=20)
+    nickname = models.CharField(verbose_name="닉네임", max_length=16)
+    phone_number = models.CharField(verbose_name="전화번호", max_length=11)
+    
+    def __str__(self):
+        return f'[{self.nickname}] 프로필'
+
+
 class Receiver(models.Model):
     image = models.ImageField(verbose_name='이미지', blank=True, default='')
-    nickname = models.CharField(verbose_name='닉네임', max_length=10)
+    nickname = models.CharField(verbose_name='닉네임', max_length=16)
     anniversary_yy = models.IntegerField(verbose_name="기념일 연")
     anniversary_mm = models.IntegerField(verbose_name="기념일 월")
     anniversary_dd = models.IntegerField(verbose_name="기념일 일")
