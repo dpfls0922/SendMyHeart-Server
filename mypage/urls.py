d6127365dfbf8cd3ed52a55a230cc87f178e372f
yerinlee <dpfls0922@naver.com>
2023-11-16 22:36:02 +0900

    [Add] #1 이미지 담을 media 경로 추가

--- mypage/urls.py
+++ mypage/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from rest_framework.viewsets import ModelViewSet
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import *
 
 urlpatterns = [
     path('receiver/all/', get_receivers, name='receivers_all'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- sendMyHeart/settings.py
+++ sendMyHeart/settings.py
@@ -149,6 +149,12 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 
+# media 파일
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
