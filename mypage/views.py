189068c51cd7f540beb6525f8da49d9b32522bd2
yerinlee <dpfls0922@naver.com>
2023-11-17 00:02:03 +0900

    [Feat] #7 안부친구 수정

--- mypage/views.py
+++ mypage/views.py
@@ -1,4 +1,6 @@
+import json
 from django.shortcuts import render, get_object_or_404
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import *
@@ -10,10 +12,21 @@ def get_receivers(request):
         receiver = Receiver.objects.all().order_by('-id')
         serializer = ReceiverSerializer(receiver, many=True, context={'request': request})
         return Response(serializer.data)
-    
-@api_view(['GET'])
+
+
+@api_view(['GET', 'PUT'])
 def get_receiver(request, receiver_id):
     if request.method == 'GET':
         receiver = get_object_or_404(Receiver, pk=receiver_id)
         serializer = ReceiverSerializer(receiver, context={'request': request})
         return Response(serializer.data)
+    
+    
+    elif request.method == 'PUT':
+        receiver = get_object_or_404(Receiver, pk=receiver_id)
+        serializer = ReceiverSerializer(receiver, data = request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
