5eeb7edc2f53abdcf2237cd07c0f0365bd61d05b
yerinlee <dpfls0922@naver.com>
2023-11-17 01:02:41 +0900

    [Feat] #11 내 프로필 모델 serializer 구현

--- mypage/serializers.py
+++ mypage/serializers.py
@@ -2,6 +2,13 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from .models import *
 
+
+class ProfileSerializer(ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
 class ReceiverSerializer(ModelSerializer):
     class Meta:
         model = Receiver
