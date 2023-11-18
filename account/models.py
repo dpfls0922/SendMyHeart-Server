de0518d0ebf003286e9b834755f5681e6d77e6c0
윤서희 <appleid21@sookmyung.ac.kr>
2023-11-19 00:01:02 +0900

    [Feat] #13 회원가입 구현

--- account/models.py
+++ account/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    phone = models.CharField(verbose_name='전화번호', max_length=11)
