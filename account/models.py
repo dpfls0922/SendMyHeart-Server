3bc8bfd8485e40fabc2c6cfe6bd8ccbe45fef12f
윤서희 <appleid21@sookmyung.ac.kr>
2023-11-21 21:45:29 +0900

    Fix: 프로필 사진 설정

--- account/models.py
+++ account/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    profile_image = models.ImageField(blank=True, null=True)
     phone = models.CharField(verbose_name='전화번호', max_length=11)
