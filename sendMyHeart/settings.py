d254b3ad5d3d84066960997473bbdd7557f48744
윤서희 <appleid21@sookmyung.ac.kr>
2023-11-21 20:59:53 +0900

    [Fix] settings.py 수정

--- sendMyHeart/settings.py
+++ sendMyHeart/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
     'mypage',
 
     'rest_framework',
+    'conrsheaders',
 ]
 
 AUTH_USER_MODEL = 'account.User'
@@ -81,7 +82,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000')
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'sendMyHeart.urls'
 
