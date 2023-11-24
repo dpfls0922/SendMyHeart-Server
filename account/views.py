1bccf4582840b4a498a8b53ff1bff2bfc3eb5662
윤서희 <appleid21@sookmyung.ac.kr>
2023-11-24 12:08:00 +0900

    [Fix] conflict 해결

--- account/views.py
+++ account/views.py
@@ -1,86 +1,115 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.forms import AuthenticationForm
-from .forms import SignUpForm, LoginForm
+from django.shortcuts import get_object_or_404, render
+import jwt
+
+from sendMyHeart.settings import SECRET_KEY
+from .serializers import *
 from .models import User
-from django.contrib.auth import login, logout
-from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
+from rest_framework.response import Response
 
-@csrf_exempt
 # 회원가입
-def signup(request):
-    # GET 요청 시 HTML 응답
-    if request.method == 'GET':
-        form = SignUpForm()
-        context = {'form' : form}
-        return render(request, 'signup.html', context)
-    
-    # POST 요청 시 데이터 확인 후 회원 생성
-    else:
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False) # 바로 저장 X
-            request.session['user'] = {
-                'email' : user.email,
-                'phone' : user.phone,
-            }
-            user.save()
-            return redirect('account:additional_info')
+class SignupAPIView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data, context={'request':request})
+        if serializer.is_valid():
+            user = serializer.save()
+
+            # jwt 토큰 접근
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user" : serializer.data,
+                    "message" : "register access",
+                    "token" : {
+                        "access" : access_token,
+                        "refresh" : refresh_token,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+
+            # kwt 토큰 > 쿠키에 저장
+            res.set_cookie("access", access_token, httponly=True)
+            res.set_cookie("refresh", refresh_token, httponly=True)
+
+            return res
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AuthAPIView(APIView):
+    # 유저 정보 확인
+    def get(self, request):
+        try:
+            # access token을 decode 해서 유저 id 추출 => 유저 식별
+            access = request.COOKIES['access']
+            payload = jwt.decode(access, SECRET_KEY, algorithms=['HS256'])
+            pk = payload.get('user_id')
+            user = get_object_or_404(User, pk=pk)
+            serializer = UserSerializer(instance=user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        except(jwt.exceptions.ExpiredSignatureError):
+            # 토큰 만료 시 토큰 갱신
+            data = {'refresh': request.COOKIES.get('refresh', None)}
+            serializer = TokenRefreshSerializer(data=data)
+            if serializer.is_valid(raise_exception=True):
+                access = serializer.data.get('access', None)
+                refresh = serializer.data.get('refresh', None)
+                payload = jwt.decode(access, SECRET_KEY, algorithms=['HS256'])
+                pk = payload.get('user_id')
+                user = get_object_or_404(User, pk=pk)
+                serializer = UserSerializer(instance=user)
+                res = Response(serializer.data, status=status.HTTP_200_OK)
+                res.set_cookie('access', access)
+                res.set_cookie('refresh', refresh)
+                return res
+            raise jwt.exceptions.InvalidTokenError
+
+        except(jwt.exceptions.InvalidTokenError):
+            # 사용 불가능한 토큰일 때
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    # 로그인
+    def post(self, request):
+    	# 유저 인증
+        user = authenticate(
+            email=request.data.get("email"), password=request.data.get("password")
+        )
+        # 이미 회원가입 된 유저일 때
+        if user is not None:
+            serializer = UserSerializer(user)
+            # jwt 토큰 접근
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "login success",
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+            # jwt 토큰 => 쿠키에 저장
+            res.set_cookie("access", access_token, httponly=True)
+            res.set_cookie("refresh", refresh_token, httponly=True)
+            return res
         else:
-            return redirect('account:signup')
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-# 프로필 및 닉네임 설정  
-def additional_info(request):
-    if 'user' not in request.session:
-        return redirect('account:signup')
-    
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        profile_image = form.cleaned_data['profile_image']
-
-        user_info = request.session['user']
-        user_info['username'] = username
-        user_info['profile_image'] = profile_image
-
-        user = User.objects.get(email=user_info['email'])
-        user.username = username
-        user.profile_image = profile_image
-        user.save()
-
-        return redirect('account:login')
-    else:
-        form = SignUpForm()
-    return render(request, 'additional_info.html', {'form' : form})
-
-@csrf_exempt
-# 로그인
-def login_view(request):
-    if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get("email")
-            raw_password = form.cleaned_data.get("password")
-            msg = "올바른 유저 이메일과 패스워드를 입력하세요."
-            try:
-                user = User.objects.get(email=email)
-            except User.DoesNotExist:
-                pass
-            else:
-                if user.check_password(raw_password):
-                    msg = None
-                    login(request, user)
-                    return redirect('mypage:create_receivers')
-                    # return render(request, "base.html") -> 로그아웃 버튼
-    else:
-        msg = None
-        form = LoginForm()
-    return render(request, "login.html", {"form": form, "msg": msg})
-
-@csrf_exempt
-# 로그아웃
-def logout_view(request):
-    # 데이터 유효성 검사
-    if request.user.is_authenticated:
-        logout(request)
-    return redirect('account:signup')
+    # 로그아웃
+    def delete(self, request):
+        # 쿠키에 저장된 토큰 삭제 => 로그아웃 처리
+        response = Response({
+            "message": "Logout success"
+            }, status=status.HTTP_202_ACCEPTED)
+        response.delete_cookie("access")
+        response.delete_cookie("refresh")
+        return response
--- sendMyHeart/settings.py
+++ sendMyHeart/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -69,10 +70,57 @@ INSTALLED_APPS = [
     'mypage',
 
     'rest_framework',
+    'rest_framework_simplejwt',
+
     'corsheaders',
 ]
 
 AUTH_USER_MODEL = 'account.User'
+
+# jwt토큰 인증 설정
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        # 'rest_framework.permissions.IsAuthenticated', # 인증된 사용자만 접근
+        # 'rest_framework.permissions.IsAdminUser', # 관리자만 접근
+        'rest_framework.permissions.AllowAny', # 누구나 접근
+    ),
+}
+
+# 추가 설정
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
