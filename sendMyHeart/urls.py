3dc16d5ed8061c08582e2a6f12d203325c0dbcd8
dpfls0922 <dpfls0922@naver.com>
2023-11-27 17:58:24 +0900

    [Add] url 경로 설정

--- sendMyHeart/urls.py
+++ sendMyHeart/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from account import urls as account_url
 from main import urls as main_url
 from mypage import urls as mypage_url
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +12,5 @@ urlpatterns = [
     path('main/', include(main_url)),
     path('mypage/', include(mypage_url)),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
